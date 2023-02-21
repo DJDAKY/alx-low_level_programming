1ed9e2e9deb74dbcef85495e9852931098821aec
DJDAKY <kelvin.agyare20@gmail.com>
2023-02-21 11:07:15 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,12 @@
  * main - Entry point
  * Return: Always 0
  */
- 
- int main(void)
 
- {
+int main(void)
 
- 	_putchar('_');
+{	
+
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
