1a581529a9555d4730b2ae71b02cdfcff205ed23
DJDAKY <kelvin.agyare20@gmail.com>
2023-02-28 17:08:05 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-   void time_table(void);
-    return (0);
+	times_table();
+	return (0);
 }
